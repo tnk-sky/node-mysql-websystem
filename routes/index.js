36379398f5b651a877bd17da4329cac78547abc2
なかそ <142368624+tnk-sky@users.noreply.github.com>
2026-08-12 14:37:52 +0000

    サインイン状態によってトップページの内容を切り替えよう

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const knex = require('../db/knex');
-// const mysql = require('mysql');
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password',
-//   database: 'todo_app'
-// });
-
-// router.get('/', function (req, res, next) {
-//   knex("tasks")
-//     .select("*")
-//     .then(function (results) {
-//       console.log(results);
-//       res.render('index', {
-//         title: 'ToDo App',
-//         todos: results,
-//       });
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//       res.render('index', {
-//         title: 'ToDo App',
-//       });
-//     });
-// });
-
-// router.post('/', function (req, res, next) {
-//   const todo = req.body.add;
-//   knex("tasks")
-//     .insert({user_id: 1, content: todo})
-//     .then(function () {
-//       res.redirect('/')
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//       res.render('index', {
-//         title: 'ToDo App',
-//       });
-//     });
-// });
-
-// router.use('/signup', require('./signup'));
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
@@ -54,26 +5,27 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  console.log(`isAuth: ${isAuth}`);
-
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const todo = req.body.add;
   knex("tasks")
     .insert({user_id: 1, content: todo})
@@ -84,6 +36,7 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
