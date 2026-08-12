ffdd803c265cb41319869f4d7fa26ecabe4e778d
なかそ <142368624+tnk-sky@users.noreply.github.com>
2026-08-12 14:23:42 +0000

    サインイン機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,10 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  console.log(`isAuth: ${isAuth}`);
+
   knex("tasks")
     .select("*")
     .then(function (results) {
@@ -85,5 +89,6 @@ router.post('/', function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
